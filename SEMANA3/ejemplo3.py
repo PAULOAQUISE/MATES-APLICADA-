9976ac0a998c429acff7d2e0dcb747ece59eca13
PAULOAQUISE <102945311+PAULOAQUISE@users.noreply.github.com>
2022-04-20 20:57:23 -0500

    clase

---
+++ SEMANA3/ejemplo3.py
@@ -0,0 +1,11 @@
+a=5
+print("a")
+print(a)
+
+b=3
+c=a+b
+print(c)
+print(a-b)
+print(a*b)
+print(8%2)
+
---
+++ SEMANA3/ejemplo4.py
@@ -0,0 +1,7 @@
+a=5
+print(a)
+print("el valor de la bariable es:",a)
+
+a=3
+print(a)
+print ("el balor de la variable es:",a)
